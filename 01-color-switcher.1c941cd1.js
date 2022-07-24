23d24ddc89764b274dff282e9b376cbe9f138e8d
clocwerk <65974401+clocwerk@users.noreply.github.com>
2022-07-24 06:45:27 +0000

    Deploying to gh-pages from @ clocwerk/goit-js-hw-09@9386d7268a1e57b97e7a8af0693145f68ae2d52c 🚀

---
+++ 01-color-switcher.1c941cd1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d=null;t.addEventListener("click",(function(a){t.disabled=!0,e.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),500)})),e.addEventListener("click",(function(a){t.disabled=!1,e.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.1c941cd1.js.map
---
+++ 01-color-switcher.ee6619c4.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(a){t.disabled=!0,e.disabled=!1,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),500)})),e.addEventListener("click",(function(a){t.disabled=!1,e.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.ee6619c4.js.map
